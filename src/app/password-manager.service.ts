a942f24ae34eac5db98dd456f1cee3e6bfcf2275
abhijeetpalanki <ajpalanki@gmail.com>
2024-02-19 14:48:10 -0600

    add and load sites with service injection

--- src/app/password-manager.service.ts
+++ src/app/password-manager.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PasswordManagerService {
+  firestore = inject(Firestore);
 
-  constructor() { }
+  addSite(data: object) {
+    const dbInstance = collection(this.firestore, "sites");
+    return addDoc(dbInstance, data);
+  }
+
+  getAllSites() {
+    const dbInstance = collection(this.firestore, "sites");
+    return collectionData(dbInstance, { idField: "id" });
+  }
 }
--- src/app/site-list/site-list.component.ts
+++ src/app/site-list/site-list.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { PasswordManagerService } from '../password-manager.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-site-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './site-list.component.html',
   styleUrl: './site-list.component.css'
 })
-export class SiteListComponent {
+export class SiteListComponent implements OnInit {
+  passwordManagerService = inject(PasswordManagerService);
+  allSites!: Observable<Array<any>>;
 
+  ngOnInit(): void {
+    this.getAllSites();
+  }
+
+
+
+  onSubmit(values: NgForm) {
+    this.passwordManagerService.addSite(values)
+      .then(() => {
+        console.log("Data saved successfully!");      
+      })
+      .catch(err => {
+        console.log(err);        
+      });
+  }
+
+  getAllSites() {
+    this.allSites = this.passwordManagerService.getAllSites();
+  }
 }
